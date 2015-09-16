3fbc2c107c61a541e7fd11331bb279014428fbe2
Adrián de la Rosa Martín <adriantovick@gmail.com>
2015-09-16 16:12:34 +0200

    Dependencias sin vendoring, helper para errores

--- auvasa.go
+++ auvasa.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 	"strconv"
 
-	"gitlab.com/adrm/hipsterbot/Godeps/_workspace/src/github.com/yhat/scrape"
+	"github.com/yhat/scrape"
 
-	"gitlab.com/adrm/hipsterbot/Godeps/_workspace/src/golang.org/x/net/html"
-	"gitlab.com/adrm/hipsterbot/Godeps/_workspace/src/golang.org/x/net/html/atom"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 
-	"gitlab.com/adrm/hipsterbot/Godeps/_workspace/src/golang.org/x/text/encoding/charmap"
-	"gitlab.com/adrm/hipsterbot/Godeps/_workspace/src/golang.org/x/text/transform"
+	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/transform"
 )
 
 // ProximoBus describe un tiempo de llegada para un bus concreto.
@@ -27,13 +27,13 @@ func Get(parada int) (string, []ProximoBus, error) {
 	resp, err := http.Get("http://www.auvasa.es/paradamb.asp?codigo=" +
 		strconv.Itoa(parada))
 	if err != nil {
-		return "", nil, errors.New("Error al conectar con el servidor de AUVASA.")
+		return fail("Error al conectar con el servidor de AUVASA.")
 	}
 
 	rInUTF8 := transform.NewReader(resp.Body, charmap.Windows1252.NewDecoder())
 	root, err := html.Parse(rInUTF8)
 	if err != nil {
-		return "", nil, errors.New("Error en la respuesta de AUVASA.")
+		return fail("Error en la respuesta de AUVASA.")
 	}
 
 	headers := scrape.FindAll(root, scrape.ByTag(atom.H1))
@@ -49,9 +49,16 @@ func Get(parada int) (string, []ProximoBus, error) {
 		})
 	}
 
+	if len(resultados) == 0 {
+		return fail("No hay tiempos para la parada especificada.")
+	}
+
 	if len(headers) >= 2 {
 		return scrape.Text(headers[1]), resultados, nil
 	}
-	return "", nil, errors.New("La respuesta de AUVASA no se corresponde con lo esperado.")
+	return fail("La respuesta de AUVASA no se corresponde con lo esperado.")
+}
 
+func fail(msg string) (string, []ProximoBus, error) {
+	return "", nil, errors.New(msg)
 }
